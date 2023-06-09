8e21bed483be0dfdddb04fd192e48b5a44a0aac0
iwan0210 <iwancuah13@gmail.com>
2023-06-09 20:52:11 +0700

    first commit

---
+++ latihan1.bat
@@ -0,0 +1,7 @@
+@echo off
+title Latihan 1 - Menampilkan Identitas diri
+echo NIM            : 20121078
+echo Nama           : M. ARIEF KURNIAWAN
+echo No WA          : 082215207561
+echo Alamat         : Wonoyoso RT.22 RW.08 Kec. Buaran, Kab. Pekalongan
+pause
